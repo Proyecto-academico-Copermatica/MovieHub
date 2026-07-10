5c2689b4c80f463507aba9b3500bf5dd0b45de77
P4bM4rx <pabloskimarxante@gmail.com>
2026-07-10 11:55:07 +0200

    Primer commit: Creación de la BDD y configuración de la misma

---
+++ MovieHubAPI/Models/GeneroModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class GeneroModel
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Nombre { get; set; } = string.Empty;
+
+    // Propiedad de navegación
+    public ICollection<PeliculaGeneroModel> PeliculaGeneros { get; set; } = new List<PeliculaGeneroModel>();
+}
