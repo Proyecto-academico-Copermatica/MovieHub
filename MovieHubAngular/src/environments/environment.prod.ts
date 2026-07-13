66b4119663ebdf56b8d6c43923ff4434cd778010
Carlos P. <dawcarlospereira@gmail.com>
2026-07-13 17:49:33 +0200

    feat(config): crear environment.ts / .prod.ts con apiUrlBase + fileReplacements en angular.json

---
+++ MovieHubAngular/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrlBase: 'https://localhost:7154/api'
+};
---
+++ MovieHubAngular/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrlBase: 'https://localhost:7154/api'
+};
